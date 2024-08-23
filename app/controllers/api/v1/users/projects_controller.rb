b0e5b78563b8af1372e7cf18b351bd4291c08436
Shadbox21 <hr@shadbox.com>
2024-08-23 20:33:57 +0530

    added data

--- app/controllers/api/v1/users/projects_controller.rb
+++ app/controllers/api/v1/users/projects_controller.rb
@@ -1,18 +1,19 @@
+
 class Api::V1::ProjectsController < ApplicationController
- def index
- 	user_projects = UserProject.where(user_id: params[:user_id])
-    render json: serialize_response
- end
+  def index
+   user_projects = UserProject.where(user_id: params[:user_id])
+   render json: serialize_response(user_projects)
+  end
 
- def serialize_response
- 	response = []
- 	user_projects.each do |user_project|
- 	   response << project_hash(user_project)
- 	end
- 	response
- end
+def serialize_response(user_projects)
+   response = []
+   user_projects&.each do |user_project|
+      response << project_hash(user_project)
+   end
+   response
+end
 
- def project_hash(user_project)
- 	{project_id: user_project.project_id, progress: user_project.progess, options: user_project.options}
- end
+   def project_hash(user_project)
+      {project_name: user_project.project&.name, progress: user_project.progress, options: user_project.options}
+   end
 end
